d6eace38ddf1e8c7fcefa4cc7a297d67673f1261
duonghuynh <trungduong22021619@gmail.com>
2023-02-09 00:41:01 +0700

    Add cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5001",
+  origin: "http://localhost:10000",
 };
 app.use(cors(corsOptions));
 const swaggerJsDoc = require("swagger-jsdoc");
