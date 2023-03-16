1fda76450e18561ad9a177522fc4fbdd0476f649
duonghuynh <trungduong22021619@gmail.com>
2023-03-16 13:18:32 +0700

    Change host

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -197,7 +197,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5001/api/auth/google/callback',
+      callbackURL:
+        'https://rental-vehicle-na07.onrender.com/api/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       //get the user data from google
